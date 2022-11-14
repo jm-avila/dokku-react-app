0a090db8cd737e4a849876909d06f5635155fca8
jose-avila-007 <jose.avila@rookiewise.com>
2022-11-14 17:52:07 +0100

    Empty app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{process.env.REACT_APP_NAME || "NO NAME"}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
